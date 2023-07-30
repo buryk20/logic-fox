6d51da1aecfb7880ed4770c9be54fc18ae1b1275
buryk20 <burykseriy@gmail.com>
2023-07-30 12:06:40 +0300

    footer

--- src/js/headerMenu.js
+++ src/js/headerMenu.js
@@ -4,10 +4,17 @@ export let headerMenu = function() {
     if(headerWrp) {
         const btnMenu = headerWrp.querySelector('[data-btn-menu]');
         const menuList = headerWrp.querySelector('[data-menu-list-wrp]');
+        const btnBaclCall = document.querySelector('[data-btn-back-call]');
+        console.log(btnBaclCall);
 
         btnMenu.addEventListener('click', function () {
             this.classList.toggle('active');
             menuList.classList.toggle('active');
+        });
+
+        btnBaclCall.addEventListener('click', () => {
+            menuList.classList.remove('active');
+            btnMenu.classList.remove('active');
         })
     }
 }
