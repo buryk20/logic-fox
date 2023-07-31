e5cfd6bf7fc9377f0f83237c5235bd95216312a5
buryk20 <73736907+buryk20@users.noreply.github.com>
2023-07-31 17:51:47 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import Inspect from 'vite-plugin-inspect'
+import Inspect from 'vite-plugin-inspect';
+import assetsRename from 'vite-plugin-assets-rename';
 
 export default {
   plugins: [
-    Inspect()
+    Inspect(),
   ],
   base: './',
 }
